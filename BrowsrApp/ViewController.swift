af0e284c9a7713a2b80841ae6424946da38812e1
Sunil Zishan <sunilzishan85@gmail.com>
2023-07-24 17:06:32 +0200

    Initial Commit

---
+++ BrowsrApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BrowsrApp
+//
+//  Created by Sunil Zishan on 24.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
